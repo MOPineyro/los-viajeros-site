06059e68bc9d86d4dcc49663dd9923a728763110
Manuel Pineyro <manuel.o.pineyro@gmail.com>
2018-08-14 12:19:29 -0400

    fix mobile menu toggle for store select

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,6 +1,11 @@
 // This is where it all goes :)
 $(document).ready(function () {
-    $('.order-online-select').magnificPopup({
-        type: 'inline'
+    $(".order-online-select").magnificPopup({
+        type: "inline"
     });
+
+    $(".order-online-select").on("click", function () {
+        $('#mainnav-mobi').slideToggle(300);
+        $('.btn-menu').toggleClass('active');
+    })
 })
